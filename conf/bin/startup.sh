623478fd1b90dc41e8ff55c9422d5e1edef75de7
zyc <zyc@hasor.net>
2017-03-15 17:37:59 +0800

    1.增加启动 nginx

--- conf/bin/startup.sh
+++ conf/bin/startup.sh
@@ -5,5 +5,7 @@ mkdir -p "$WEBSITE_HOME/logs/nginx"
 mkdir -p "$WEBSITE_HOME/logs/tomcat"
 mkdir -p "$WEBSITE_HOME/logs/rsf/"
 
+/usr/local/nginx/sbin/nginx
+
 cd $CATALINA_HOME
 exec "catalina.sh" "$@"
